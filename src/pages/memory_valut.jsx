f0af88d2cd6eed3224321fcb65a6dd8ebd069058
rahulrr-coder <rahulrr5002@protonmail.com>
2024-12-20 18:05:16 +0530

    Memory vault

--- src/pages/memory_valut.jsx
+++ src/pages/memory_valut.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-
+import './mv.css'
+import diwali from '../assets/Blog/Diwali.jpeg'
 
 const Mv = () => {
 
     return(
         <>
-        <h1>MV</h1>
-        </>
+        <div className="mv-container">
+        <h1>Memory Vault</h1>
+    </div>
+    <div class='diwali'>
+        <img src={diwali} alt="" />
+    </div>
+        <div>
+        <div ></div>
+        <div> </div>
+        </div>
+    </>
+    
+
     );
 }
+
+
 export default Mv;
